774f8ef1c29e254692b6bdb21feb81ecc22b7bc2
felipe.siqueira <felipesiqgodoy@gmail.com>
2024-03-13 19:17:23 -0300

    chore: add swaggerOptions

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,13 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+      defaultModelsExpandDepth: 0,
+    },
+  });
 
   await app.listen(3000);
 }
